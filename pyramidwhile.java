724af563fcd9b213a3a59d31c2323a8868b47bc6
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-10 10:34:15 +0530

    update

--- pyramidwhile.java
+++ pyramidwhile.java
@@ -1,29 +1,30 @@
 package com.orthofx;
 import java.util.Scanner;
-public class pyramidwhile {
+public class pyramidfor {
 	public static void main(String[] args)
 	{
-    Scanner read=new Scanner(System.in);
-	System.out.println("enter height of pyramid");
-	int s=read.nextInt();
-	int i=0,j=0,h,k=1;
-	h=s+s-1;
-	while(i<s)
-	{
-		while(j<=h)
+		Scanner read=new Scanner(System.in);
+		System.out.println("enter height of pyramid");
+		int s=read.nextInt();
+		int h=s+s-1;
+		int k=1,i=0,j=1;
+		while(i<s)
 		{
-			int n=(h-k)/2;
-		if(j<=n || j>(n+k))
-			System.out.print(" ");
-		else
-			System.out.print("*");
-		j=j+1;
+			while(j<=h)
+			{
+				int n=(h-k)/2;
+				if(j<=n || j>(n+k))
+					System.out.print(" ");
+				else
+					System.out.print("*");
+				j++;
+			}
+			System.out.println(" ");
+			k=k+2;
+			i++;		
+			}
+		}
+		
 	}
-		System.out.println("  ");
-		i=i+1;
-		k=k+2;
 
-}
-	}
-}
-	
+
